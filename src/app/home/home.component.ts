41df621ced6dea8f4456386b9de647c55fef137f
David Starr <david@elegantcode.com>
2025-04-28 10:47:50 -0700

    model binding working from hard coded component data.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic can go here
+  }
+
+  // Add any additional methods or properties needed for the component
+  
 
 }
